8297093cfebeb0cdf9120988331b08dc77d683f0
Alex Alvarado <Alexjalva@gmail.com>
2021-03-27 12:23:49 -0700

    latest changes to local files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,10 +5,16 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
 
     try {
-      res.render('home-page', { 
-         
-        logged_in: req.session.logged_in 
-      });
+      res.render('home-page');
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/login', async (req, res) => {
+
+    try {
+      res.render('login');
     } catch (err) {
       res.status(500).json(err);
     }
