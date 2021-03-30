dc868e99a77592e0fcaac61d04e3a1c259099050
Miguel Alcantar <malcantar185@gmail.com>
2021-03-29 20:00:01 -0700

    seeds updated

--- seeds/userData.js
+++ seeds/userData.js
@@ -1,1 +1,24 @@
 const { User } = require('../models');
+
+const userData = [
+    {
+       "name": "Miguel",
+       "password": "Secret123" 
+    },
+    {
+        "name": "Carlo",
+        "password": "Secret213" 
+     },
+     {
+        "name": "Alex",
+        "password": "Secret321" 
+     }, {
+        "name": "Angel",
+        "password": "123Secret" 
+     },
+     {
+        "name": "Jose",
+        "password": "Secret312" 
+     }
+]
+module.exports = seedUsers;
