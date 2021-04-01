3dcf47969615eda799b505e87fdae3be955438db
Alex Alvarado <Alexjalva@gmail.com>
2021-03-31 19:18:51 -0700

    add item page and form working

--- controllers/api/item-routes.js
+++ controllers/api/item-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Item, Category } = require('../../models');
-const withAuth =  require('../../utils/auth');
+const withAuth = require('../../utils/auth');
 const cloudinary = require('cloudinary').v2;
 
 // The `/api` endpoint
@@ -47,25 +47,28 @@ router.post('/', withAuth, async (req, res) => {
   console.log(req.body);
   try {
     await cloudinary.uploader.upload(
-      'http://mobileimages.lowes.com/product/converted/190873/190873002673.jpg', 
-      {public_id: 'fridge'}, 
-      function(error, result) { 
-        console.log(result) 
+      'http://mobileimages.lowes.com/product/converted/190873/190873002673.jpg',
+      { public_id: 'fridge' },
+      function (error, result) {
+        console.log(result)
       }
     );
     console.log("cloudinary worked");
     console.log(req.body.category);
-  let category = await Category.findOne({ where: {name: req.body.category}});
-  console.log("tried to find");
-  console.log(category);
-  if(category == null){
-    console.log("trying to create");
-      category = await Category.create({ name: req.body.category});
-  }
-  category = findOne
-  console.log("past cloudinary");
-  console.log(category);
-  const catID = category.id;
+    const catName = req.body.category;
+    console.log(catName);
+    let category = await Category.findOne({ where: { name: catName } });
+    console.log("tried to find");
+    console.log(category);
+    if (category == null) {
+      console.log("trying to create");
+      category = await Category.create({ name: catName});
+    }
+    console.log("past cloudinary");
+    //const categoryData = category.map((category) => category.get({ plain: true }));
+
+    const catID = category.dataValues.id;
+    console.log(catID);
     const newItem = await Item.create({
       name: req.body.name,
       dimensions: req.body.dimensions,
@@ -86,7 +89,7 @@ router.post('/', withAuth, async (req, res) => {
 router.put('/:id', withAuth, async (req, res) => {
   // update product data
   try {
-    const updateItem = await Item.update(req.body, 
+    const updateItem = await Item.update(req.body,
       {
         where: {
           id: req.params.id
@@ -106,8 +109,8 @@ router.delete('/:id', withAuth, async (req, res) => {
         id: req.params.id
       }
     });
-    if(!itemData) {
-      res.status(404).json({message: 'No item found with this id! '});
+    if (!itemData) {
+      res.status(404).json({ message: 'No item found with this id! ' });
       return;
     }
     res.status(200).json(itemData);
